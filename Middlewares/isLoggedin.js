7932eddba5639ea7e8b0b01f7ddc8fe2aad56d53
AhmedHammad <hammad97official@gmail.com>
2019-05-20 14:22:50 +0200

    Change loggedin convention

--- Middlewares/isLoggedin.js
+++ Middlewares/isLoggedin.js
@@ -1,14 +1,14 @@
 const dbConnection = require(__dirname + '/../Database/connection');
 
 module.exports = (req, res, next) => {
-  if(!req.cookies.id) req.isLoggedin = false;
+  if(!req.cookies.id) req.loggedin = false;
   else{
     let sql = "Select * FROM User WHERE id = ?";
     dbConnection.query(sql, req.cookies.id, (err, result)=>{
       if(err) throw err;
       else{
-        if(result.length === 0) req.isLoggedin = false;
-        else req.isLoggedin = true;
+        if(result.length === 0) req.loggedin = false;
+        else req.loggedin = true;
       }
     })
   }
