8c0c58f0a749fcfdcbea09378f22c4a785ff793f
AhmedHammad <hammad97official@gmail.com>
2019-05-20 18:34:39 +0200

    Fix query syntax

--- Services/addFriend.js
+++ Services/addFriend.js
@@ -2,7 +2,7 @@ const dbConnection = require(__dirname + '/../Database/connection');
 
 module.exports = (req, res) => {
   let sql = "INSERT INTO Request (Sender, Reciever) VALUES (?, ?)";
-  dbConnection.query(sql, req.cookies.id, req.body.friendId, (err, result) => {
+  dbConnection.query(sql, [req.cookies.id, req.body.friendId], (err, result) => {
     if(err) throw err;
     else{
       res.send(true)
--- Services/registerUser.js
+++ Services/registerUser.js
@@ -1,7 +1,56 @@
 const dbConnection = require(__dirname + '/../Database/connection');
+const shortid = require('shortid');
+const bcrypt = require('bcrypt')
 
 module.exports = (req, res) => {
+  let userId = shortid.generate();
+  let imgName = null;
 
-  console.log(req.body);
-  console.log(req.files);
+  if (req.files === null || req.files === undefined){
+    if (req.body.gender = "male") imgName = 'maledefault.png';
+    else imgName = 'femaledefault.png';
+  }
+  else{imgName = userId + '.' + req.files.img.name.split('.').pop();}
+
+  if (req.body.nickName === '') {req.body.nickName = req.body.firstName + ' ' + req.body.lastName}
+  if (req.body.marital === undefined){req.body.marital = null;}
+
+  let hash = bcrypt.hashSync(req.body.password, 8);
+  // Insert User data
+  let sql = `INSERT INTO User VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  dbConnection.query(sql, [userId, req.body.firstName, req.body.lastName, req.body.nickName, hash, req.body.email, req.body.gender,
+  req.body.birthdate, req.body.hometown, req.body.marital, req.body.aboutme, imgName],
+  (err, result) => {
+     if (err) {throw err; res.send("Error, wrong data"); return;}
+     else{
+       if (!req.body.phones.trim() === ''){
+         // Insert phone numbers
+         let phoneNums = req.body.phones.split(' ');
+         sql = `INSERT INTO Phone VALUES`;
+         let phoneList = [];
+         for (let i=0; i<phoneNums.length; i++){
+           phoneList.push(userId)
+           phoneList.push(phoneNums[i])
+           sql += ` (?, ?),`
+         }
+         sql = sql.slice(0, -1);
+         dbConnection.query(sql, phoneList, (err, result) => {
+           if (err) {
+             throw err;
+             sql = `DELETE FROM User WHERE User.Id = ?`
+             dbConnection.query(sql, userId, (err, result) => {
+               if (err) throw err;
+             })
+              res.send("Error, wrong data"); return;
+           }
+           else{
+             if(req.files.img !== undefined){
+               // Insert image
+               req.files.img.mv(__dirname + '/../views/assets/images')
+             }
+           }
+         })
+       }
+     }
+  });
 }
