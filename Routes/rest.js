bea0f4fe2985551e37deaa5f92b5893de6d06365
AhmedHammad <hammad97official@gmail.com>
2019-05-20 02:31:05 +0200

    Setup views + Done with navbar, footer, homepage

--- Routes/rest.js
+++ Routes/rest.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = require('express').Router();
 const bodyParser = require('body-parser');
+const loggedinMiddleware = require(__dirname + '/../Middlewares/isLoggedin')
 
 //BodyParser
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', (req, res) => {res.send("Hi there!")})
+router.get('/', loggedinMiddleware, (req, res) => {
+  res.render('homepage', {loggedin : req.isLoggedin})
+})
 
 module.exports = router;
