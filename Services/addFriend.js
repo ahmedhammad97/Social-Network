6338654af4d6cb32d40eec447eb1106b9a2222ca
AhmedHammad <hammad97official@gmail.com>
2019-05-21 02:35:22 +0200

    Done with friends recommendation functionality

--- Services/addFriend.js
+++ Services/addFriend.js
@@ -1,9 +1,9 @@
 const dbConnection = require(__dirname + '/../Database/connection');
 
 module.exports = (req, res) => {
-  let sql = "INSERT INTO Request (Sender, Reciever) VALUES (?, ?)";
-  dbConnection.query(sql, [req.cookies.id, req.body.friendId], (err, result) => {
-    if(err) throw err;
+  let sql = "INSERT INTO Request (Sender, Reciever) VALUES (?, ?), (?, ?)";
+  dbConnection.query(sql, [req.cookies.id, req.body.friendId, req.body.friendId, req.cookies.id], (err, result) => {
+    if(err) {console.log(err); res.render('error')}
     else{
       res.send(true)
     }
--- Services/friendsRecommender.js
+++ Services/friendsRecommender.js
@@ -1,8 +1,8 @@
 const dbConnection = require(__dirname + '/../Database/connection');
 
 module.exports = (req, res) => {
-  let sql = `SELECT User.Id, User.NickName, User.Picture FROM User INNER JOIN Friendship ON User.Id = Friendship.Id1
-             WHERE User.Id NOT = ? AND Friendship.Id2 NOT = ?`;
+  let sql = `SELECT User.Id, User.NickName, User.Picture FROM User LEFT JOIN Friendship ON User.Id = Friendship.Id1
+             WHERE User.Id <> ? OR Friendship.Id2 <> ? LIMIT 5;`;
   dbConnection.query(sql, [req.cookies.id, req.cookies.id], (err, result) => {
     if(err) throw err;
     else{
