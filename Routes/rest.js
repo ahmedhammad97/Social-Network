db839c27438579a8ba675618361758c39c2fd58c
Mohamed Tarek <mtarek.22796@gmail.com>
2019-05-21 07:31:06 +0200

    friendlist

--- Routes/rest.js
+++ Routes/rest.js
@@ -1,36 +1,48 @@
-const express = require('express');
-const router = require('express').Router();
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const registerUser = require(__dirname + '/../Services/registerUser')
-const loginUser = require(__dirname + '/../Services/loginUser')
-const recommender = require(__dirname + '/../Services/friendsRecommender')
-const addFriend = require(__dirname + '/../Services/addFriend')
-
+const express = require("express");
+const router = require("express").Router();
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const registerUser = require(__dirname + "/../Services/registerUser");
+const loginUser = require(__dirname + "/../Services/loginUser");
+const recommender = require(__dirname + "/../Services/friendsRecommender");
+const addFriend = require(__dirname + "/../Services/addFriend");
+const friendList = require(__dirname + "/../Services/friendList");
 //BodyParser
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', (req, res) => {
-  res.render('homepage', {loggedin : req.loggedin})
-})
+router.get("/", (req, res) => {
+  res.render("homepage", { loggedin: req.loggedin });
+});
 
-router.get('/register', (req, res) => {
-  if (req.loggedin) res.redirect('/')
-  else res.render('register', {loggedin : false})
-})
+router.get("/register", (req, res) => {
+  if (req.loggedin) res.redirect("/");
+  else res.render("register", { loggedin: false });
+});
 
-router.get('/login', (req, res) => {
-  if (req.loggedin) res.redirect('/')
-  else res.render('login', {loggedin : false})
-})
+router.get("/login", (req, res) => {
+  if (req.loggedin) res.redirect("/");
+  else res.render("login", { loggedin: false });
+});
 
-router.post('/register', fileUpload(), urlencodedParser, registerUser)
+router.get("/myprofile", (req, res) => {
+  if (req.loggedin) res.render("myprofile", { loggedin: req.loggedin });
+  else res.render("login", { loggedin: false });
+});
 
-router.post('/login', urlencodedParser, loginUser)
+router.get("/friendlist", (req, res) => {
+  if (req.loggedin) res.render("friendlist", { loggedin: req.loggedin });
+  else res.render("login", { loggedin: false });
+});
 
-router.post('/friendsrecommendations', recommender)
+router.post("/register", fileUpload(), urlencodedParser, registerUser);
 
-router.post('/addfriend', jsonParser, addFriend)
+router.post("/login", urlencodedParser, loginUser);
+
+router.post("/friendsrecommendations", recommender);
+
+router.post("/addfriend", jsonParser, addFriend);
+
+router.post("/friendList", friendList);
 
 module.exports = router;
---
+++ Services/checkprofile.js
@@ -0,0 +1,3 @@
+var url_Id = url.searchParams.get("id");
+var cookie_Id = document.cookie.split[0].slice[3];
+console.log(cookie_Id);
