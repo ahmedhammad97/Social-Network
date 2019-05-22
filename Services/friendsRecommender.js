f8fcd8419483d2b74d96d72212c88edef36a1bcd
AhmedHammad <hammad97official@gmail.com>
2019-05-22 17:59:40 +0200

    Enhance recommendation module to suggest only non-friends

--- Services/friendsRecommender.js
+++ Services/friendsRecommender.js
@@ -1,9 +1,30 @@
 const dbConnection = require(__dirname + '/../Database/connection');
 
 module.exports = (req, res) => {
-  let sql = `SELECT User.Id, User.NickName, User.Picture FROM User LEFT JOIN Friendship ON User.Id = Friendship.Id1
-             WHERE User.Id <> ? OR Friendship.Id2 <> ? OR Friendship.Id1 <> ? LIMIT 6;`;
-  dbConnection.query(sql, [req.cookies.id, req.cookies.id, req.cookies.id], (err, result) => {
+  let sql = `SELECT DISTINCT u1.Id, u1.NickName, u1.Picture
+              FROM User u1
+              LEFT JOIN (
+                  SELECT ID2 AS id
+                  FROM Friendship
+                  WHERE ID1 = ?
+                  UNION ALL
+                  SELECT ID1
+                  FROM Friendship
+                  WHERE ID2 = ?
+                  UNION ALL
+                  SELECT Sender
+                  FROM Request
+                  WHERE Reciever = ?
+                  UNION ALL
+                  SELECT Reciever
+                  FROM Request
+                  WHERE Sender = ?
+                  ) ux
+              ON ux.id = u1.id
+              WHERE ux.id IS null
+              and u1.Id <> ?
+              LIMIT 9`;
+  dbConnection.query(sql, [req.cookies.id, req.cookies.id, req.cookies.id, req.cookies.id, req.cookies.id], (err, result) => {
     if(err) throw err;
     else{
       res.send(result)
