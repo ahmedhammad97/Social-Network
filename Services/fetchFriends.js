cbf8e7ec8368eb0c9a58b3f06ba379ddb0cc6b37
AhmedHammad <hammad97official@gmail.com>
2019-05-22 19:09:15 +0200

    Fix friendlist

--- Services/fetchFriends.js
+++ Services/fetchFriends.js
@@ -1,5 +1,13 @@
-const dbConnection = require(__dirname + '/../Database/connection');
+const dbConnection = require(__dirname + "/../Database/connection");
 
 module.exports = (req, res) => {
-  
-}
+  let sql = `SELECT DISTINCT User.Id, User.NickName, User.Picture FROM User
+            INNER JOIN Friendship ON User.Id = Friendship.Id1
+            WHERE Friendship.Id2 = ?`;
+  dbConnection.query(sql, [req.cookies.id], (err, result) => {
+    if (err) throw err;
+    else {
+      res.render('friendlist', {"data": result, "loggedin": req.loggedin})
+    }
+  });
+};
